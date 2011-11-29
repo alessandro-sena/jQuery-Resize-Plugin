76dd881d5d2cbf3ed6ef38e30b536383ded2f2bd
Alessandro Sena <veneinzuela@gmail.com>
2011-11-29 11:24:23 -0400

    The function name was changed to avoid conflicts with the resize jQuery event, some bugs related to css were fixed

--- jquery.resize.js
+++ jquery.resize.js
@@ -1,9 +1,10 @@
 /*
 * Resize, A resize plugin for jQuery
-* Intructions: $(seletor).resize(max_size)
+* Intructions: $(seletor).resizeImg(max_size)
 * By: Carlos Alessandro Sena de Freitas - veneinzuela@gmail.com
-* Version: 1.0
-* Updated: September 1st, 2011
+* Especial Thanks to: Marc - m33.tmn@gmail.com
+* Version: 1.1
+* Updated: November 29, 2011
 *
 * Licensed under the GNU - General Public License 
 * You may obtain a copy of the License at
@@ -12,7 +13,7 @@
 *
 */
 (function( $ ){
-	$.fn.resize = function(max_size) 
+	$.fn.resizeImg = function(max_size) 
 	{
    	return this.each(function() 
 		{  
@@ -21,7 +22,7 @@
 			{
 				//Create a container div
 				var div_container = $("<div></div>");
-				div_container.css({ 'height': max_size, 'width': max_size, 'position': 'absolute'});
+				div_container.css({ 'height': max_size, 'width': max_size, 'position': 'relative'});
 
 				//Get the new size of the image
 				if ($(this).height() > $(this).width()) 
@@ -36,8 +37,8 @@
 			  	}
 					
 				//Centralize the image
-				var top  = Math.floor((120-h)/2);
-				var left = Math.floor((120-w)/2);
+				var top  = Math.floor((max_size-h)/2);
+				var left = Math.floor((max_size-w)/2);
 			
 				//Resize the image and put it in the container div
 			  	$(this).css({ 'height': h, 'width': w, 'top': top, 'left': left, 'position': 'absolute'});
